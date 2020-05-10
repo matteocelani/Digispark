d96c636d66c52f3c92011c736a5f11f7c72a9216
Matteo Celani <matteo.celani@studio.unibo.it>
2020-05-10 17:14:56 +0200

    Keyboard Layout Italian

---
+++ Italian-keyboard-layout/test/test.ino
@@ -0,0 +1,7 @@
+#include "DigiKeyboard.h"
+
+void setup() {
+	DigiKeyboard.print("abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789 \\|!\"$%&/()=?^<>[]{}@#,;.:-_");
+}
+
+void loop() {}
